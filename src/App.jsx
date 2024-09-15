50c0168b95c49ef9c9ea81dd3e2ba936b113b83f
vinayBagde <vinaybagde0@gmail.com>
2024-09-15 09:06:17 +0530

    create comments form component

--- src/App.jsx
+++ src/App.jsx
@@ -1,9 +1,11 @@
 import "./App.css";
 import Form from "./components/form/Form.jsx";
+import CommentsForm from "./components/commentsForm/CommentsForm.jsx";
 export default function App() {
   return (
     <>
-      <Form />
+      {/* <Form /> */}
+      <CommentsForm />
     </>
   );
 }
